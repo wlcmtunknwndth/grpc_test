8b5eafb12677981220eda7121217e546e58214bf
Артем Петров <fittedorangeofficial@mail.ru>
2024-06-20 01:55:23 +0300

    added some methods

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -4,24 +4,85 @@ import (
 	"context"
 	ssov1 "github.com/wlcmtunknwndth/proto_only_test/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+const emptyValue = 0
+
+type Auth interface {
+	Login(ctx context.Context,
+		email string, password string, appID int,
+	) (token string, err error)
+	RegisterNewUser(ctx context.Context,
+		email string, password string) (userID int64, err error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
 
 type serverAPI struct {
 	ssov1.UnimplementedAuthServer
+	auth Auth
 }
 
-func Register(gRPC *grpc.Server) {
-	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+func Register(gRPC *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{auth: auth})
 }
 
 func (s *serverAPI) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
-	return &ssov1.LoginResponse{Token: req.GetEmail()}, nil
+	// need more tough email check
+	if err := validateLogin(req); err != nil {
+		return nil, err
+	}
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		// TODO: ...
+		return nil, status.Error(codes.Internal, "internal server error")
+	}
+
+	return &ssov1.LoginResponse{Token: token}, nil
 }
 
 func (s *serverAPI) Register(ctx context.Context, req *ssov1.RegisterRequest) (*ssov1.RegisterResponse, error) {
-	panic("implement me")
+	if err := validateRegister(req); err != nil {
+		return nil, err
+	}
+
+	userID, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		// TODO: ...
+		return nil, status.Error(codes.Internal, "internal server error")
+	}
+
+	return &ssov1.RegisterResponse{UserId: userID}, nil
 }
 
 func (s *serverAPI) IsAdmin(ctx context.Context, req *ssov1.IsAdminRequest) (*ssov1.IsAdminResponse, error) {
 	panic("implement me")
+}
+
+func validateLogin(req *ssov1.LoginRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "no email specified")
+	}
+
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "no password specified")
+	}
+
+	if req.GetAppId() == emptyValue {
+		return status.Error(codes.InvalidArgument, "no app id specified")
+	}
+
+	return nil
+}
+
+func validateRegister(req *ssov1.RegisterRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "no email specified")
+	}
+
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "no password specified")
+	}
+	return nil
 }
