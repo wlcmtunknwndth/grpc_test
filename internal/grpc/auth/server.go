f1451c329d28cf8349ee9b520ec2dcff4fed5a52
Артем Петров <fittedorangeofficial@mail.ru>
2024-06-20 02:06:43 +0300

    and some more

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -57,7 +57,17 @@ func (s *serverAPI) Register(ctx context.Context, req *ssov1.RegisterRequest) (*
 }
 
 func (s *serverAPI) IsAdmin(ctx context.Context, req *ssov1.IsAdminRequest) (*ssov1.IsAdminResponse, error) {
-	panic("implement me")
+	if err := validateIsAdmin(req); err != nil {
+		return nil, err
+	}
+
+	isAdmin, err := s.auth.IsAdmin(ctx, req.GetUserId())
+	if err != nil {
+		//TODO: ...
+		return nil, status.Error(codes.Internal, "internal server error")
+	}
+
+	return &ssov1.IsAdminResponse{IsAdmin: isAdmin}, nil
 }
 
 func validateLogin(req *ssov1.LoginRequest) error {
@@ -84,5 +94,13 @@ func validateRegister(req *ssov1.RegisterRequest) error {
 	if req.GetPassword() == "" {
 		return status.Error(codes.InvalidArgument, "no password specified")
 	}
+	return nil
+}
+
+func validateIsAdmin(req *ssov1.IsAdminRequest) error {
+	if req.GetUserId() == emptyValue {
+		return status.Error(codes.InvalidArgument, "no user id specified")
+	}
+
 	return nil
 }
